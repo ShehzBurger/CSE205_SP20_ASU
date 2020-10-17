4d7463d9b4f2aa579b54cb830b494690441bcbc3
ShehzBurger <cougar59912@gmail.com>
2020-10-17 16:52:09 -0700

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,34 @@
+
+        Assignment #: 3
+        Name: Shehzad Anwar
+        StudentID: 1216575295
+        Lecture: 1:30PM
+
+        1. PUBLIC - Software engineers shall act consistently with the public interest.
+
+        -If a software engineer finds a bug in a piece of code for a product that will go out to the public
+        in a matter of weeks, then they have a duty to fix the bugs as the public will be using broken code if they do not.
+
+        2. PRODUCT - Software engineers shall ensure that their products and related modifications meet the highest professional standards possible.
+
+        -If an Android OS develepor finds flaws within his/her custom OS, it is their responsibility to solve the flaw, which
+        may include stability issues, performance issues and bugs in the system. The product must be in the best condition it can
+        be in or else the develepor is not fulfilling his/her duty.
+
+        3. COLLEAGUES - Software engineers shall be fair to and supportive of their colleagues.
+
+        -Software engineers are not obligated to share their code with anyone, but it is their duty to help and support their colleagues.
+        For example, if an engineer sees someone in their workplace who is struggling, in order to adhere to the code, they should
+        provide some assistance to them.
+
+        4. PROFESSION - Software engineers shall advance the integrity and reputation of the profession consistent with the public interest.
+
+        -In order to protect the reputation and integrity of their professional workplace, software engineers need
+        to do whatever it takes to fulfill their customers' needs. For example, in an Apple store, if someone comes in with
+        a bootlooping iPhone, it is the Apple worker's job to figure out what to do to uphold the company's status.class
+
+        5. MANAGEMENT - Software engineering managers and leaders shall subscribe to and promote an ethical approach to the management of software development and maintenance.
+
+        -There always needs to be leaders among a team and that is no different in software engineering. For example if a team does not know what UI
+        design choices to make, a software engineer needs to take charge and make the decision. This will make for a clearer path for future design choices
+        and make it easier for the team to work together in the future.
